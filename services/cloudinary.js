9bcf379f68d42da734c5aec992b6711fcb8f78d8
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-07-01 15:05:36 +0100

    vercel deployment

--- services/cloudinary.js
+++ services/cloudinary.js
@@ -10,8 +10,8 @@ export const uploadToS3 = async (file,filename, mimeType) => {
         const s3 = new S3Client({
             region: process.env.AWS_REGION,
             credentials:{
-                accessKeyId: process.env.AWS_ACCESS_KEY,
-                secretAccessKey: process.env.AWS_SECRET_KEY
+                accessKeyId: process.env.AWS_ACCESS,
+                secretAccessKey: process.env.AWS_SECRET
             }
         })
 
